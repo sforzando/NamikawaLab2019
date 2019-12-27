801abde5f8a246b045081a302770e6fe3246b066
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2019-12-28 04:19:14 +0900

    :+1: Improve downloadFromInstagramByHashtag.sh

---
+++ downloadFromInstagramByHashtag.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eu
+
+HASHTAG=wabisabi
+NUM2DL=30000
+TARGET_DIR=dataset/$HASHTAG
+TARGET_COLORSPACE=sRGB
+
+# Check Dependency
+which identify > /dev/null
+
+# Create Directory
+mkdir -p $TARGET_DIR
+
+# Download
+#pip3 install instaLooter
+#python3 -m instalooter hashtag $HASHTAG $TARGET_DIR --template {id}_{width}x{height} --num-to-dl $NUM2DL
+
+# Check Resolution
+find $TARGET_DIR -type f -not -name "*_1080x1080.jpg" | xargs rm -f
+
+# Check Colorspace
+for jpg in $(find $TARGET_DIR -type f -name "*.jpg"); do
+  colorspace=$(identify -format "%[colorspace]" $jpg)
+  if [ $colorspace != $TARGET_COLORSPACE ]; then
+    echo "NG: $jpg is $colorspace"
+    rm -f $jpg
+  fi
+done
+
+# Rename
+DIGIT=100001
+for jpg in $(find $TARGET_DIR -type f -name "*.jpg"); do
+  basename=$(basename $jpg)
+  echo $TARGET_DIR/${DIGIT:1}_$basename
+  DIGIT=$((++DIGIT))
+done
--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from pathlib import Path
 
 app = Flask(__name__)
+
+
+def get_images():
+    dataset_path = Path('/home/shin/NamikawaLab2019/dataset/wabisabi')
+    print(dataset_path)
 
 
 @app.route('/')
@@ -34,4 +40,5 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    get_images()
